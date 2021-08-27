30444ea4319d8079b92ff17f03e41416433e4d41
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2021-08-27 09:23:46 -0300

    Fixing cluster index

--- src/cluster.py
+++ src/cluster.py
@@ -23,7 +23,7 @@ def clusterize(path: str, filename: str) -> pandas.DataFrame:
     clusterer.fit(df)
     soft_clusters = hdbscan.all_points_membership_vectors(clusterer)
     lines, columns = soft_clusters.shape
-    header = ["index"] + df.index
+    header = ["index"] + list(df.index)
     output_name = os.path.join(output_path, filename)
     
     matrix = numpy.zeros((columns,lines+1)).astype("object")
