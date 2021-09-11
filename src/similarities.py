65796b30b3b4e8086693ebca66539ee372742146
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2021-09-11 18:08:55 -0300

    Using checkpoint

--- src/similarities.py
+++ src/similarities.py
@@ -2,17 +2,22 @@ import sys
 import os
 import pandas
 import pickle
+import asyncio
 import multiprocessing
 from scipy.spatial.distance import jensenshannon
 
 directory = sys.argv[1]
 output_path = os.path.join(directory, "similarity.txt")
 processes = int(multiprocessing.cpu_count()-1)
-args = []
+checkpoint = os.path.join(directory, "checkpoint.pkl")
 with open(os.path.join(directory, "features.pkl"), "rb") as f:
     features = pickle.load(f)["max"]
 
 print(f"loaded {len(features)} features")
+
+async def write_checkpoint(compare_args):
+    with open(checkpoint, "wb") as f:
+        pickle.dump(compare_args, f)
 
 def similarity(df, filename, filename_compare):
     basename = filename.split(".csv")[0]
@@ -36,26 +41,41 @@ def similarity(df, filename, filename_compare):
         sim.flush()
     print(f"wrote {basename} and {basename_compare} similarity {similarity} to file")
 
-print(f"openning all csv files...")
+async def main():
+    if os.path.exists(checkpoint):
+        print("getting files remaining from checkpoint...")
+        with open(checkpoint, "rb") as f:
+            args = pickle.load(f)
+    else:
+        print("openning all csv files...")
+        args = []
+        for filename in os.listdir(directory):
+            if filename.endswith(".csv") and not output_path.endswith(filename):
+                args.append((directory, filename))
 
-for filename in os.listdir(directory):
-    if filename.endswith(".csv") and not output_path.endswith(filename):
-        args.append((directory, filename))
+    with open(output_path, "w") as sim:
+        sim.write("gene,compare,similarity")
 
-with open(output_path, "w") as sim:
-    sim.write("gene,compare,similarity")
+    print(f"starting similarity check with {len(args)} combinations...")
 
+    # combinations_with_replacement 2 x 2
+    for i, path in enumerate(args):
+        filepath = os.path.join(*path)
+        df = pandas.read_csv(filepath, index_col=0, header=None, skiprows=1).T
+        compare_args = [(df, path[1], f) for d, f in args[i:]]
+        print(f"comparing {i+1} from {len(args)} sequences...")
+        asyncio.ensure_future(write_checkpoint(compare_args))
+        with multiprocessing.Pool(processes) as pool:
+            pool.starmap(
+                similarity,
+                compare_args
+            )
+    os.remove(checkpoint)
 
-print(f"starting similarity check with {len(args)} combinations...")
-
-# combinations_with_replacement 2 x 2
-for i, path in enumerate(args):
-    filepath = os.path.join(*path)
-    df = pandas.read_csv(filepath, index_col=0, header=None, skiprows=1).T
-    compare_args = [(df, path[1], f) for d, f in args[i:]]
-    print(f"comparing {i+1} from {len(args)} sequences...")
-    with multiprocessing.Pool(processes) as pool:
-        pool.starmap(
-            similarity,
-            compare_args
-        )
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    
+    # Let's also finish all running tasks:
+    pending = asyncio.Task.all_tasks()
+    loop.run_until_complete(asyncio.gather(*pending))
