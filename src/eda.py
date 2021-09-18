8753d3b1d3752cb1b3ad0b7dd45305de4b4d0b36
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2021-09-18 19:37:47 -0300

    Adding prints and save all file

--- src/eda.py
+++ src/eda.py
@@ -6,9 +6,13 @@ directory = sys.argv[1]
 output_path = os.path.join(directory, "variance.txt")
 args = []
 
-for filename in os.listdir(directory):
+for i, filename in enumerate(os.listdir(directory)):
     if filename.endswith(".csv") and not output_path.endswith(filename):
+        print(f"adding file {i+1} to list...")
         args.append(
             pandas.read_csv(os.path.join(directory, filename), index_col="cluster"))
+print("concating...")
 df = pandas.concat(args, copy=False, index=1)
 df.T.var().to_csv(output_path)
+print("Saving compressed...")
+df.to_csv(output_path + ".gzip")
