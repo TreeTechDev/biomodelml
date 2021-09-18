6b32ffce2bebb879cd5148536a10294d9c6ebd07
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2021-09-18 19:57:18 -0300

    Concating to use less memory

--- src/eda.py
+++ src/eda.py
@@ -10,10 +10,10 @@ args = []
 for i, filename in enumerate(os.listdir(directory)):
     if filename.endswith(".csv") and not output_path.endswith(filename):
         print(f"adding file {i+1} to list...")
-        args.append(
-            pandas.read_csv(os.path.join(directory, filename), index_col="cluster"))
-print("concating...")
-df = pandas.concat(args, copy=False, index=1)
+        df = pandas.concat(
+            [df, pandas.read_csv(os.path.join(directory, filename), index_col="cluster")],
+        copy=False, index=1)
+
 df.T.var().to_csv(output_path)
 print("Saving compressed...")
 df.to_parquet(output_all_path)
