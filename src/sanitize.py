f336145578ea349388c5712a0469a30fb57e0d04
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2022-11-11 14:53:42 -0300

    ignoring if already sanitized

--- src/sanitize.py
+++ src/sanitize.py
@@ -12,15 +12,16 @@ SEQ_TYPES = SeqTypeStruct(N=NUCLEOTIDE_SYMBOLS, P=PROTEIN_SYMBOLS)
 def remove_unrelated_sequences(seq_path: str, seq_type):
     if seq_type not in SEQ_TYPES.__dataclass_fields__:
         raise IOError("Only sequence type N or P accepted")
+    if os.path.exists(seq_path + ".sanitized"):
+        print(f"already sanitized file f{seq_path + '.sanitized'}")
+        return
     with open(seq_path) as handle:
         sequences = SeqIO.parse(handle, "fasta")
         sanitized_seqs = []
-        for k, s in enumerate(sequences):
-            if os.path.exists(seq_path + ".sanitized"):
-                continue
+        for s in sequences:
             alphabet = set(s.seq)
             if alphabet.issubset(getattr(SEQ_TYPES, seq_type)):
                 sanitized_seqs.append(s)
 
-    print(f"writing {len(sanitized_seqs)} of {k+1} sequences")
+    print(f"writing {len(sanitized_seqs)} sequences")
     SeqIO.write(sanitized_seqs, seq_path + ".sanitized", "fasta")
